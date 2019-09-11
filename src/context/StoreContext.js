24bf4a7325d9651a33b7604032bedb8cb4d504f8
Shawn yap <shawnycx@gmail.com>
2019-09-11 15:21:00 -0500

    another prod fix attempt

--- src/context/StoreContext.js
+++ src/context/StoreContext.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Client from 'shopify-buy';
 
 const client = Client.buildClient({
-  domain: process.env.GATSBY_SHOP_NAME,
+  domain: `${process.env.GATSBY_SHOP_NAME}.myshopify.com`,
   storefrontAccessToken: process.env.GATSBY_SHOPIFY_ACCESS_TOKEN,
 });
 
