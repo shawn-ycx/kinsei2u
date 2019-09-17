8f34aaed15537093d5d011eb415e9bb8dd6da0e8
Shawn yap <shawnycx@gmail.com>
2019-09-17 10:29:19 -0500

    Product page carousel on images

--- src/templates/ProductPage/index.js
+++ src/templates/ProductPage/index.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-import ProductForm from '../../components/ProductForm';
-import { Img } from '../../utils/styles';
+import Slider from 'react-slick';
 
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { Paper } from '@material-ui/core';
 
+import ProductForm from '../../components/ProductForm';
+import { Img } from '../../utils/styles';
+
+import '../../assets/carousel-styles/slick.css';
+import '../../assets/carousel-styles/slick-theme.css';
+
 const ProductPage = ({ data }) => {
   const product = data.shopifyProduct;
+  const sliderConfig = {
+    customPaging: function(i) {
+      return (
+        <a>
+          <Img
+            fluid={product.images[i].localFile.childImageSharp.fluid}
+            alt={product.title}
+          />
+        </a>
+      );
+    },
+    fade: true,
+    dots: true,
+    dotsClass: 'slick-dots slick-thumb',
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
   return (
     <>
@@ -22,13 +46,17 @@ const ProductPage = ({ data }) => {
             margin="0 auto"
             border="1px solid #ececea"
           >
-            {product.images.map(x => (
-              <Img
-                fluid={x.localFile.childImageSharp.fluid}
-                key={x.id}
-                alt={product.title}
-              />
-            ))}
+            <Slider {...sliderConfig}>
+              {product.images.map(x => (
+                <div>
+                  <Img
+                    fluid={x.localFile.childImageSharp.fluid}
+                    key={x.id}
+                    alt={product.title}
+                  />
+                </div>
+              ))}
+            </Slider>
           </Box>
         </Grid>
         <Grid xs={12} md={6} item>
